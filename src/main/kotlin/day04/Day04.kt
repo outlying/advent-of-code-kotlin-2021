7b1725b074a86cf8931d2a21951a0c5b295459e0
Iwo Polański <outlying@gmail.com>
2021-12-03 09:17:00 +0100

    Future

---
+++ src/main/kotlin/day04/Day04.kt
@@ -0,0 +1,20 @@
+package day04
+
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val testInput = readInput("Day00_test")
+    check(part1(testInput) == 0)
+
+    val input = readInput("Day00")
+    println(part1(input))
+    println(part2(input))
+}
